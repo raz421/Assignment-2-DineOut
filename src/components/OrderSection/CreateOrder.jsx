db1c2c7bb1a1d04fa7f5772595b1c4a7e90e6578
raz421 <maharazbhuiyan3@gmail.com>
2025-05-14 13:06:32 +0600

    initially create order done

--- src/components/OrderSection/CreateOrder.jsx
+++ src/components/OrderSection/CreateOrder.jsx
@@ -1,8 +1,21 @@
-import ChikenNuggets from "../../assets/chicken.svg";
-import HamBurger from "../../assets/hamburger.svg";
-import Pizza from "../../assets/pizza.svg";
-import Sandwich from "../../assets/submarine.svg";
-export default function CreateOrder() {
+import { useState } from "react";
+import { FaMinus, FaPlus } from "react-icons/fa6";
+const nextId = 1;
+export default function CreateOrder({ items, onToggle, onOrder }) {
+  const [name, setName] = useState("");
+  let [totalMoney, setTotalMoney] = useState(0);
+  let [count, setCount] = useState(0);
+  const handlePlus = (money) => {
+    setTotalMoney((totalMoney += money));
+    setCount((count = count + 1));
+  };
+  const handleMinus = (money) => {
+    if (totalMoney > 0) {
+      setTotalMoney((totalMoney -= money));
+      setCount((count = count - 1));
+    }
+    return totalMoney;
+  };
   return (
     <div className="bg-cardbg rounded-lg p-6 h-[calc(100vh_-_130px)]">
       <h2 className="text-xl font-bold mb-1">CREATE ORDER</h2>
@@ -15,6 +28,10 @@ export default function CreateOrder() {
       <div className="mb-4">
         <label className="block text-sm font-medium mb-2">Customer Name</label>
         <input
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
           type="text"
           className="w-full bg-gray-700 bg-opacity-50 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-primary transition-all duration-300"
         />
@@ -25,126 +42,58 @@ export default function CreateOrder() {
         <label className="block text-sm font-medium mb-2">Choose Items</label>
         <div className="items-container">
           {/* <!-- Item 1 --> */}
-          <div className="bg-gray-700 bg-opacity-30 rounded-md p-3 mb-3 flex justify-between items-center hover:bg-opacity-40 transition-all duration-300">
-            <div className="flex items-center">
-              <div className="w-12 h-12   flex items-center justify-center mr-3">
-                <img src={HamBurger} alt="Hamburger" className="w-10 h-10" />
+          {items.map((item) => (
+            <div
+              key={item.id}
+              className="bg-gray-700 bg-opacity-30 rounded-md p-3 mb-3 flex justify-between items-center hover:bg-opacity-40 transition-all duration-300"
+            >
+              <div className="flex items-center">
+                <div className="w-12 h-12   flex items-center justify-center mr-3">
+                  <img src={item.icon} alt={item.name} className="w-10 h-10" />
+                </div>
+                <div>
+                  <h3 className="font-medium">{item.name}</h3>
+                  <p className="text-xs text-gray-400">BDT {item.money}</p>
+                </div>
               </div>
-              <div>
-                <h3 className="font-medium">Hamburger</h3>
-                <p className="text-xs text-gray-400">BDT 300</p>
-              </div>
+              <button className="w-8 h-8 bg-gray-800 hover:bg-primary rounded-full flex items-center justify-center transition-colors duration-300">
+                {item.isAdd ? (
+                  <FaPlus
+                    color="green"
+                    onClick={() => {
+                      handlePlus(item.money);
+                      onToggle(item);
+                    }}
+                  />
+                ) : (
+                  <FaMinus
+                    color="red"
+                    onClick={() => {
+                      handleMinus(item.money);
+                      onToggle(item);
+                    }}
+                  />
+                )}
+              </button>
             </div>
-            <button className="w-8 h-8 bg-gray-800 hover:bg-primary rounded-full flex items-center justify-center transition-colors duration-300">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-green-500"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
-
-          {/* <!-- Item 2 --> */}
-          <div className="bg-gray-700 bg-opacity-30 rounded-md p-3 mb-3 flex justify-between items-center hover:bg-opacity-40 transition-all duration-300">
-            <div className="flex items-center">
-              <div className="w-12 h-12 flex items-center justify-center mr-3">
-                <img
-                  src={ChikenNuggets}
-                  alt="Chicken Nuggets"
-                  className="w-10 h-10"
-                />
-              </div>
-              <div>
-                <h3 className="font-medium">Chicken Nuggets</h3>
-                <p className="text-xs text-gray-400">BDT 300</p>
-              </div>
-            </div>
-            <button className="w-8 h-8 bg-gray-800 hover:bg-primary rounded-full flex items-center justify-center transition-colors duration-300">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-green-500"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
-
-          {/* <!-- Item 3 --> */}
-          <div className="bg-gray-700 bg-opacity-30 rounded-md p-3 mb-3 flex justify-between items-center hover:bg-opacity-40 transition-all duration-300">
-            <div className="flex items-center">
-              <div className="w-12 h-12 flex items-center justify-center mr-3">
-                <img
-                  src={Sandwich}
-                  alt="Submarine Sandwich"
-                  className="w-10 h-10"
-                />
-              </div>
-              <div>
-                <h3 className="font-medium">Submarine Sandwich</h3>
-                <p className="text-xs text-gray-400">BDT 300</p>
-              </div>
-            </div>
-            <button className="w-8 h-8 bg-gray-800 hover:bg-primary rounded-full flex items-center justify-center transition-colors duration-300">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-red-500"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
-
-          {/* <!-- Item 4 --> */}
-          <div className="bg-gray-700 bg-opacity-30 rounded-md p-3 mb-3 flex justify-between items-center hover:bg-opacity-40 transition-all duration-300">
-            <div className="flex items-center">
-              <div className="w-12 h-12 bg-yellow-600 rounded-md flex items-center justify-center mr-3">
-                <img src={Pizza} alt="Pizza slices" className="w-10 h-10" />
-              </div>
-              <div>
-                <h3 className="font-medium">Pizza slices</h3>
-                <p className="text-xs text-gray-400">BDT 300</p>
-              </div>
-            </div>
-            <button className="w-8 h-8 bg-gray-800 hover:bg-primary rounded-full flex items-center justify-center transition-colors duration-300">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-green-500"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
+          ))}
         </div>
       </div>
 
       {/* <!-- Place Order Button --> */}
-      <button className="w-full bg-primary hover:bg-opacity-90 text-white font-medium py-3 rounded-full transition-all duration-300 hover:shadow-lg transform hover:-translate-y-1">
-        Place Order (BDT 100)
+      <button
+        onClick={() => {
+          onOrder({
+            id: nextId + 1,
+            customerName: name,
+            itemsOrder: count,
+            amount: totalMoney,
+            status: "PENDING",
+          });
+        }}
+        className="w-full bg-primary hover:bg-opacity-90 text-white font-medium py-3 rounded-full transition-all duration-300 hover:shadow-lg transform hover:-translate-y-1"
+      >
+        Place Order (BDT {totalMoney})
       </button>
     </div>
   );
---
+++ src/components/OrderSection/OrderReports.jsx
@@ -0,0 +1,58 @@
+import { BsFunnel } from "react-icons/bs";
+
+export default function OrderReports({ orders }) {
+  return (
+    <div>
+      <div className="flex justify-between">
+        <h2 className="text-xl font-bold mb-4">Order Reports</h2>
+
+        <div className="flex gap-4 items-center">
+          <BsFunnel />
+          <select className="appearance-none bg-zinc-900 accent-orange-600 border-none outline-none rounded-sm">
+            <option>All</option>
+            <option>Pending</option>
+            <option>Delivered</option>
+          </select>
+        </div>
+      </div>
+      <div className="bg-cardbg rounded-lg p-4">
+        <div className="reports-container">
+          <table className="min-w-full">
+            <thead>
+              <tr className="text-left text-sm">
+                <th className="pb-3 font-medium">ID</th>
+                <th className="pb-3 font-medium">Customer Name</th>
+                <th className="pb-3 font-medium">Items</th>
+                <th className="pb-3 font-medium">Amount</th>
+                <th className="pb-3 font-medium">Status</th>
+                <th className="pb-3 font-medium">Action</th>
+              </tr>
+            </thead>
+            <tbody className="text-sm">
+              {/* <!-- Row 1 --> */}
+              {orders.map((order) => (
+                <tr key={order.id} className="border-t border-gray-700">
+                  <td className="py-3">{order.id}</td>
+                  <td className="py-3">{order.customerName}</td>
+                  <td className="py-3">{order.itemsOrder}</td>
+                  <td className="py-3">{order.amount}</td>
+                  <td className="py-3">
+                    <span className="text-red-500">{order.status}</span>
+                  </td>
+                  <td className="py-3">
+                    <button className="bg-gray-800 hover:bg-red-600 text-xs px-3 py-1 rounded-full mr-1 transition-colors duration-300">
+                      Delete
+                    </button>
+                    <button className="bg-gray-800 hover:bg-green-600 text-xs px-3 py-1 rounded-full transition-colors duration-300">
+                      DELIVER
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
